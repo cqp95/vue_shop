04bd4b64ee94cf181fa97d80d7b31a378bf8e10b
cqp95 <chengqipeng95@foxmail.com>
2020-03-12 21:54:15 +0800

    enter

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { Form,FormItem } from 'element-ui'
+import { Input } from 'element-ui'//导入
 
-Vue.use(Button)
+Vue.use(Button)//全局
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
